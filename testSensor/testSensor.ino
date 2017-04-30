fc346d7fdaab0beab68d4a8cd58e0913d86258f7
Teerasak Sathu <maxz_drum@icloud.com>
2017-04-30 18:48:31 +0700

    ลบ .cpp

--- testSensor/testSensor.ino
+++ testSensor/testSensor.ino
@@ -1,37 +1,83 @@
-
-
 #include <StandardCplusplus.h>
-
 #include <vector>
-
+  #include <SPI.h>
+#include <WiFi.h>
 
 using namespace std;
 
-int sensor1 = 2;
-int sensor2 = 3;
+int sensor1 = 4;
+int sensor2 = 5;
 bool checkIn = false;
 bool checkIn1 = false;
+int counting =0;
 
 vector<int> val;
 
-void setup() {
-  Serial.begin(9600);
+#define WIFI_SSID "A406"
+#define WIFI_PASSWORD "Ninewmaxz406"
+
+
+
+
+
+
+// the IP address for the shield:
+IPAddress ip(192, 168, 0, 177);    
+
+char ssid[] = "A406";    // your network SSID (name) 
+char pass[] = "Ninewmaxz406"; // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+
+void setup() {  
+  // Initialize serial and wait for port to open:
+  Serial.begin(115200); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    while(true);  // don't continue
+  } 
+
+  WiFi.config(ip);
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // print your WiFi shield's IP address:
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP()); 
+
+
+
+  
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   maClick();
-//  delay(1000);
+  
 }
 void maClick() {
   int num = digitalRead(sensor1);
   int num1 = digitalRead(sensor2);
 
 
-  //
+  
   if (num == 0 && checkIn == false ) {
     val.push_back(1);
     checkIn = true;
@@ -42,11 +88,12 @@ void maClick() {
   }
   //
   if (val[0] == 1 && val[1] == 2) {
-    Serial.println("+1");
+  counting++;    
+    Serial.println(counting);
     delay(5000);
   } else if (val[0] == 2 && val[1] == 1) {
-    
-    Serial.println("-1");
+  counting--;
+    Serial.println(counting);
     delay(5000);
   }
   Serial.print("val[0] :");
@@ -64,9 +111,9 @@ void maClick() {
 
 
   if (val.size() >= 2) {
-    val[0]=0;
-    val[1]=0;
-    val.erase(val.begin(),val.begin()+2);
+    val[0] = 0;
+    val[1] = 0;
+    val.erase(val.begin(), val.begin() + 2);
   }
 
 
